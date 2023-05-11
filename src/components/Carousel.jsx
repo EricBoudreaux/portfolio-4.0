7b19fd48a40f227fa0557cb239c89b093dca2da6
EricBoudreaux <99383061+EricBoudreaux@users.noreply.github.com>
2023-05-11 11:19:20 -0700

    Update Carousel.jsx
    Moved around carousel displays to display my more prominent projects first

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -49,7 +49,35 @@ export default function SimpleSlider() {
         </div>
       </div>
       </div>
+
       {/* Slide 2 */}
+      <div>
+      <div className='mt-8 md:flex items-center px-2 justify-between'>
+
+          <img className='rounded-[30px] basis-1/4 w-[200px] mx-auto h-[200px] md:w-[300px]  object-cover work-img' src={DragonBlade}></img>
+
+        <div className='text-white mt-4 basis-2/4 md:ml-[-25px]'>
+          <h2 className='text-[22px] font-bold'>DRAGONBLADE</h2>
+          <h6 className='font-light leading-tight text-[18px]'>
+            A website to advertise and serve as a download hub for a JRPG game that I'm designing and building.
+          </h6>
+          <div className='flex flex-col md:flex-col'>
+            <h2 className='text-[20px] font-bold'>BUILT USING</h2>
+            <h2 className='text-[18px]  uppercase'>ReactJS  Firebase  Tailwind</h2>
+          </div>
+          <div className='mt-4'>
+            <a href='https://www.dragonbladegame.com/' target='_blank'>
+              <button className='text-black bg-white btn-shadow rounded-[5px] py-2 px-6 hover:bg-black hover:text-white mb-5 duration-300 ease-in'>PREVIEW</button>
+            </a>
+            <a href='https://github.com/EricBoudreaux/dragonblade-react-website' target='_blank'>
+              <button className='text-black ml-4 bg-white btn-shadow rounded-[5px] py-2 px-6 hover:bg-black hover:text-white mb-5 duration-300 ease-in'>REPO</button>
+            </a>
+          </div>
+        </div>
+      </div>
+      </div>
+
+      {/* Slide 3 */}
       <div>
       <div className='mt-8 md:flex items-center px-2 justify-between'>
 
@@ -75,32 +103,7 @@ export default function SimpleSlider() {
         </div>
       </div>
       </div>
-      {/* Slide 3 */}
-      <div>
-      <div className='mt-8 md:flex items-center px-2 justify-between'>
 
-          <img className='rounded-[30px] basis-1/4 w-[200px] mx-auto h-[200px] md:w-[300px]  object-cover work-img' src={Photography}></img>
-
-        <div className='text-white mt-4 basis-2/4 md:ml-[-25px]'>
-          <h2 className='text-[22px] font-bold uppercase'>Photosen Photography</h2>
-          <h6 className='font-light leading-tight text-[18px]'>
-            A website built for a photography company to showcase their work and attract const clients.
-          </h6>
-          <div className='flex flex-col md:flex-row md:items-center'>
-            <h2 className='text-[20px] font-bold'>BUILT USING</h2>
-            <h2 className='text-[18px] md:ms-6 uppercase'>ReactJS  React Router  Tailwind</h2>
-          </div>
-          <div className='mt-4'>
-          <a href='https://photography-react-website.vercel.app/' target='_blank'>
-              <button className='text-black bg-white btn-shadow rounded-[5px] py-2 px-6 hover:bg-black hover:text-white mb-5 duration-300 ease-in'>PREVIEW</button>
-            </a>
-            <a href='https://github.com/EricBoudreaux/photography-react-website' target='_blank'>
-              <button className='text-black ml-4 bg-white btn-shadow rounded-[5px] py-2 px-6 hover:bg-black hover:text-white mb-5 duration-300 ease-in'>REPO</button>
-            </a>
-          </div>
-        </div>
-      </div>
-      </div>
       {/* Slide 4 */}
       <div>
       <div className='mt-8 md:flex items-center px-2 justify-between'>
@@ -127,33 +130,35 @@ export default function SimpleSlider() {
         </div>
       </div>
       </div>
-
       {/* Slide 5 */}
       <div>
       <div className='mt-8 md:flex items-center px-2 justify-between'>
 
-          <img className='rounded-[30px] basis-1/4 w-[200px] mx-auto h-[200px] md:w-[300px]  object-cover work-img' src={DragonBlade}></img>
+          <img className='rounded-[30px] basis-1/4 w-[200px] mx-auto h-[200px] md:w-[300px]  object-cover work-img' src={Photography}></img>
 
         <div className='text-white mt-4 basis-2/4 md:ml-[-25px]'>
-          <h2 className='text-[22px] font-bold'>DRAGONBLADE</h2>
+          <h2 className='text-[22px] font-bold uppercase'>Photosen Photography</h2>
           <h6 className='font-light leading-tight text-[18px]'>
-            A website to advertise and serve as a download hub for a JRPG game that I'm designing and building.
+            A website built for a photography company to showcase their work and attract const clients.
           </h6>
-          <div className='flex flex-col md:flex-col'>
+          <div className='flex flex-col md:flex-row md:items-center'>
             <h2 className='text-[20px] font-bold'>BUILT USING</h2>
-            <h2 className='text-[18px]  uppercase'>ReactJS  Firebase  Tailwind</h2>
+            <h2 className='text-[18px] md:ms-6 uppercase'>ReactJS  React Router  Tailwind</h2>
           </div>
           <div className='mt-4'>
-            <a href='https://www.dragonbladegame.com/' target='_blank'>
+          <a href='https://photography-react-website.vercel.app/' target='_blank'>
               <button className='text-black bg-white btn-shadow rounded-[5px] py-2 px-6 hover:bg-black hover:text-white mb-5 duration-300 ease-in'>PREVIEW</button>
             </a>
-            <a href='https://github.com/EricBoudreaux/dragonblade-react-website' target='_blank'>
+            <a href='https://github.com/EricBoudreaux/photography-react-website' target='_blank'>
               <button className='text-black ml-4 bg-white btn-shadow rounded-[5px] py-2 px-6 hover:bg-black hover:text-white mb-5 duration-300 ease-in'>REPO</button>
             </a>
           </div>
         </div>
       </div>
       </div>
+
+
+
     </Slider>
   );
 }
